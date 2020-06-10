0a269018b97421b9425d8625d47aad7a94b8370a
Abdul Rahim <alizada1353@android-47d7c64d23f5b9b5.home>
2020-06-10 21:28:43 +0200

    added body parser and form validator for feedback submission

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cookieSession = require('cookie-session');
 const createError = require('http-errors');
 
+const bodyParser = require('body-parser');
+
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
 
@@ -23,6 +25,8 @@ app.use(
     keys: ['gjfie3763524x', 'hks3bds32hk443'],
   })
 );
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
